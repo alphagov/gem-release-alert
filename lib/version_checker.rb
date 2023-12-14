981075f829d1aca2334e114dd5cccacc7e78f27e
PeterHattyar <peter.hattyar@digital.cabinet-office.gov.uk>
2023-12-14 09:39:33 +0000

    Added fetching methods to github, addressing issues due to discrepancies, and related tests

--- lib/version_checker.rb
+++ lib/version_checker.rb
@@ -8,20 +8,51 @@ require 'octokit'
 class HttpError < RuntimeError
 end
 
+class GemspecError < RuntimeError
+end
+
 class VersionChecker
-  def fetch_rubygems_version(gemname)
-    uri = URI("https://rubygems.org/api/v1/gems/#{gemname}.json")
+  def fetch_all_govuk_gem_versions
+    uri = URI("https://docs.publishing.service.gov.uk/gems.json")
     res = Net::HTTP.get_response(uri)
 
     raise HttpError unless res.is_a?(Net::HTTPSuccess)
 
     parsed_json = JSON.parse(res.body)
-    parsed_json['version']
+    govuk_gem_names = parsed_json.map { |gem| gem["app_name"] }
+
+    govuk_versions = govuk_gem_names.map { |name| fetch_github_version(name) }.compact
+    rubygems_versions = govuk_gem_names.map { |name| fetch_rubygems_version(name) }.compact
+    puts govuk_versions
+    puts rubygems_versions
+  end
+
+  def fetch_rubygems_version(gemname)
+    uri = URI("https://rubygems.org/api/v1/gems/#{gemname}.json")
+    res = Net::HTTP.get_response(uri)
+
+    if res.is_a?(Net::HTTPSuccess)
+      parsed_json = JSON.parse(res.body)
+      parsed_json['version']
+    end
   end
 
   def fetch_github_version(gemname)
-    payload = Octokit.contents("alphagov/#{gemname}", path: "lib/#{gemname}/version.rb")
-    Base64.decode64(payload[:content]).split('"')[1]
+    Dir.mktmpdir do |path|
+      Dir.chdir(path) do
+        clone_github_repo(gemname)
+        gemspecs = Dir.glob("*.gemspec", base: gemname)
+        if gemspecs.count == 1
+          Gem::Specification::load("#{gemname}/#{gemspecs.first}").version.to_s
+        else
+          nil
+        end
+      end
+    end
+  end
+
+  def clone_github_repo(name)
+    `git clone --recursive --depth 1 --shallow-submodules git@github.com:alphagov/#{name}.git > /dev/null 2>&1`
   end
 
   def rubygems_and_github_match?(gemname)
--- spec/version_checker_spec.rb
+++ spec/version_checker_spec.rb
@@ -7,38 +7,40 @@ RSpec.describe VersionChecker do
   it 'fetches version number of a gem from rubygems' do
     stub_rubygems_call('6.0.1')
 
-    expect(subject.fetch_rubygems_version('govuk_app_config')).to eq('6.0.1')
+    expect(subject.fetch_rubygems_version('example')).to eq('6.0.1')
   end
 
   it 'fetches version number of a gem from GitHub' do
-    stub_github_call('9.7.0')
+    stub_github_call
 
-    expect(subject.fetch_github_version('govuk_app_config')).to eq('9.7.0')
+    expect(subject.fetch_github_version('example')).to eq('1.2.3')
   end
 
   it "compares versions of gems on GitHub and rubygems and it's a match" do
-    stub_github_call('9.7.0')
-    stub_rubygems_call('9.7.0')
+    stub_github_call
+    stub_rubygems_call('1.2.3')
 
-    expect(subject.rubygems_and_github_match?('govuk_app_config')).to eq(true)
+    expect(subject.rubygems_and_github_match?('example')).to eq(true)
   end
 
   it "compares versions of gems on GitHub and rubygems and it's not a match" do
-    stub_github_call('9.7.0')
+    stub_github_call
     stub_rubygems_call('9.7.1')
 
-    expect(subject.rubygems_and_github_match?('govuk_app_config')).to eq(false)
+    expect(subject.rubygems_and_github_match?('example')).to eq(false)
   end
 
-  def stub_github_call(version)
-    repo = { content: Base64.encode64(%(module GovukAppConfig\n  VERSION = "#{version}".freeze\nend\n)) }
-    stub_request(:get, 'https://api.github.com/repos/alphagov/govuk_app_config/contents/lib/govuk_app_config/version.rb')
-      .to_return(status: 200, body: repo.to_json, headers: { 'Content-Type' => 'application/json' })
+  def stub_github_call
+    allow(subject).to receive(:clone_github_repo) do
+      FileUtils.mkdir "example"
+      FileUtils.cp File.join(__dir__, "example.gemspec"), File.join(Dir.pwd, "example")
+      # Copy a dummy gemspec into the current working directory.
+    end
   end
 
   def stub_rubygems_call(version)
     repo = { "version": version }
-    stub_request(:get, 'https://rubygems.org/api/v1/gems/govuk_app_config.json')
+    stub_request(:get, 'https://rubygems.org/api/v1/gems/example.json')
       .to_return(status: 200, body: repo.to_json, headers: {})
   end
 end
